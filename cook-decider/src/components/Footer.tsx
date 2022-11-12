a0070785b4170aebe0b6fec9d37bddf2ca720065
Llewellyn <lledsouza2209@gmail.com>
2022-11-12 15:32:52 +0530

    Updated footer height

--- cook-decider/src/components/Footer.tsx
+++ cook-decider/src/components/Footer.tsx
@@ -1,9 +1,8 @@
-import { createStyles, Container, Group, ActionIcon, Text } from "@mantine/core";
-import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram } from "@tabler/icons";
+import { createStyles, Container, Group, ActionIcon, Text, Footer } from "@mantine/core";
+import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram, IconChefHat } from "@tabler/icons";
 
 const useStyles = createStyles((theme) => ({
   footer: {
-    marginTop: 120,
     borderTop: `1px solid ${theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[2]}`,
   },
 
@@ -30,21 +29,26 @@ export function FooterSocial() {
   const { classes } = useStyles();
 
   return (
-    <div className={classes.footer}>
-      <Container className={classes.inner}>
-        <Text>Llewellyn D'souza</Text>
-        <Group spacing={0} className={classes.links} position="right" noWrap>
-          <ActionIcon size="lg">
-            <IconBrandTwitter size={18} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg">
-            <IconBrandYoutube size={18} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg">
-            <IconBrandInstagram size={18} stroke={1.5} />
-          </ActionIcon>
-        </Group>
-      </Container>
-    </div>
+    <Footer height={80}>
+      <div className={classes.footer}>
+        <Container className={classes.inner}>
+          <IconChefHat size={18} stroke={1.5} />
+
+          <Text>Llewellyn D'souza</Text>
+
+          <Group spacing={0} className={classes.links} position="right" noWrap>
+            {/* <ActionIcon size="lg">
+              <IconBrandTwitter size={18} stroke={1.5} />
+            </ActionIcon>
+            <ActionIcon size="lg">
+              <IconBrandYoutube size={18} stroke={1.5} />
+            </ActionIcon> */}
+            <ActionIcon size="lg">
+              <IconBrandInstagram size={18} stroke={1.5} />
+            </ActionIcon>
+          </Group>
+        </Container>
+      </div>
+    </Footer>
   );
 }
