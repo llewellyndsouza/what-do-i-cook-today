b019a665efbc958ea9cdb5a3357706f7840cb483
Llewellyn <lledsouza2209@gmail.com>
2022-11-12 12:53:59 +0530

    Added logger, helmet and cors

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
 
 const prisma = new PrismaClient();
 const app = express();
 
+app.use(morgan("dev"));
+app.use(cors());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
