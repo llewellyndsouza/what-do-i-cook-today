43a13eebb16a0c180cafe797371796c9c5a8e9da
Llewellyn <lledsouza2209@gmail.com>
2022-11-12 15:32:38 +0530

    Added mantine theme provider

--- cook-decider/src/App.tsx
+++ cook-decider/src/App.tsx
@@ -1,16 +1,22 @@
 import * as React from "react";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, AppShell, Container, ColorSchemeProvider, ColorScheme } from "@mantine/core";
 
 import { HeaderMiddle } from "./components/Header";
 import { FooterSocial } from "./components/Footer";
 
 function App() {
+  const [colorScheme, setColorScheme] = React.useState<ColorScheme>("light");
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <HeaderMiddle links={[{ label: "Home", link: "" }]} />
-      Content here
-      <FooterSocial />
-    </MantineProvider>
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+        <AppShell header={<HeaderMiddle links={[{ label: "Home", link: "" }]} />} footer={<FooterSocial />}>
+          <Container fluid>Content goes here</Container>
+        </AppShell>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
 
