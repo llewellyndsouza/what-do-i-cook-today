c5ee4bf95a32fbe8d88f267a3ffaa577699640a5
Llewellyn <lledsouza2209@gmail.com>
2022-11-12 15:05:23 +0530

    Updated react app imports with mantine

--- cook-decider/src/App.tsx
+++ cook-decider/src/App.tsx
@@ -1,8 +1,17 @@
 import * as React from "react";
+import { MantineProvider } from "@mantine/core";
+
+import { HeaderMiddle } from "./components/Header";
+import { FooterSocial } from "./components/Footer";
 
 function App() {
-
-  return <div className="App">Hey there</div>;
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <HeaderMiddle links={[{ label: "Home", link: "" }]} />
+      Content here
+      <FooterSocial />
+    </MantineProvider>
+  );
 }
 
 export default App;
